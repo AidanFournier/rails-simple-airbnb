44778d68ae2fc43b59e187054ab8e5af0861c724
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2021-07-10 09:13:30 +0900

    View for index added, show in progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,19 +21,19 @@ Flat.create!(
 )
 
 Flat.create!(
-  name: 'Light & Spacious Garden Flat London',
-  address: '10 Clifton Gardens London W9 1DT',
-  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
-  price_per_night: 75,
-  number_of_guests: 3
+  name: 'Arinomi-so',
+  address: '4-18-16 Minamiikebukuro',
+  description: 'Shitty place but it gets the job done',
+  price_per_night: 2000,
+  number_of_guests: 4
 )
 
 Flat.create!(
-  name: 'Light & Spacious Garden Flat London',
-  address: '10 Clifton Gardens London W9 1DT',
-  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
-  price_per_night: 75,
-  number_of_guests: 3
+  name: 'Bansosha',
+  address: '1-2-23 Yotsuya Kamicho, Kawasaki City',
+  description: 'Sharehouse with views of Costco',
+  price_per_night: 10000,
+  number_of_guests: 20
 )
 
 puts "Flats built! 🏘"
